1ef974550b96e536f600c1435c08f63c4a5b54dc
Sureshkumar SP <sp@SureshkumarsMBP.fios-router.home>
2020-05-05 13:27:41 -0400

    New project added from xcode

---
+++ datastructure cpp/datastructure cpp/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  datastructure cpp
+//
+//  Created by Sureshkumar SP on 5/5/20.
+//  Copyright © 2020 Sureshkumar SP. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
